dcda4ba5768f48cf44a94ae11b3efe29c66b3f95
Xiaomeng Yang <bit.yangxm@gmail.com>
2022-04-10 00:59:10 -0700

    Use RMSRescaler in PPO

--- rlmeta/utils/running_stats.py
+++ rlmeta/utils/running_stats.py
@@ -9,7 +9,45 @@ import torch
 import torch.nn as nn
 
 
-class RunningStats(nn.Module):
+class RunningRMS(nn.Module):
+
+    def __init__(self, size: Union[int, Tuple[int]]) -> None:
+        super().__init__()
+        self._size = (size,) if isinstance(size, int) else size
+        self._count = 0
+        self.register_buffer("_mean_square", torch.zeros(self._size))
+
+    def reset(self) -> None:
+        self._count = 0
+        self._mean_square.zero_()
+
+    def count(self) -> int:
+        return self._count
+
+    def mean_square(self) -> torch.Tensor:
+        return self._mean_square
+
+    def rms(self) -> torch.Tensor:
+        return self._mean_square.sqrt()
+
+    def rrms(self) -> torch.Tensor:
+        return self._mean_square.rsqrt()
+
+    def update(self, x: torch.Tensor) -> None:
+        size = x.size()
+        if size == self._size:
+            self._count += 1
+            self._mean_square += (x.square() - self._mean_square) / self._count
+        else:
+            assert size[1:] == self._size
+            cnt = size[0]
+            self._count += cnt
+            c = 0.0 if self._count == 0 else cnt / self._count
+            delta = x.square().mean(dim=0) - self._mean_square
+            self._mean_square += c * delta
+
+
+class RunningMoments(nn.Module):
 
     def __init__(self, size: Union[int, Tuple[int]]) -> None:
         super().__init__()
--- tests/utils/running_stats_test.py
+++ tests/utils/running_stats_test.py
@@ -7,86 +7,101 @@ import unittest
 
 import torch
 
-from rlmeta.utils.running_stats import RunningStats
+from rlmeta.utils.running_stats import RunningMoments, RunningRMS
 from tests.test_utils import TestCaseBase
 
 
-class RunningStatsTest(TestCaseBase):
+class RunningRMSTest(TestCaseBase):
 
     def setUp(self) -> None:
         self.outer_size = 10
         self.inner_size = (4, 5)
-        self.running_stats = RunningStats(self.inner_size)
+        self.running_rms = RunningRMS(self.inner_size)
         self.rtol = 1e-6
         self.atol = 1e-6
 
     def test_single_update(self) -> None:
         input = torch.rand(self.outer_size, *self.inner_size)
-        self.running_stats.reset()
+        self.running_rms.reset()
         for x in torch.unbind(input):
-            self.running_stats.update(x)
-        self.assertEqual(self.running_stats.count(), self.outer_size)
-        self.assert_tensor_close(self.running_stats.mean(),
-                                 input.mean(dim=0),
-                                 rtol=self.rtol,
-                                 atol=self.atol)
-        self.assert_tensor_close(self.running_stats.var(),
-                                 input.var(dim=0, unbiased=False),
-                                 rtol=self.rtol,
-                                 atol=self.atol)
-        self.assert_tensor_close(self.running_stats.var(ddof=1),
-                                 input.var(dim=0, unbiased=True),
-                                 rtol=self.rtol,
-                                 atol=self.atol)
-        self.assert_tensor_close(self.running_stats.std(),
-                                 input.std(dim=0, unbiased=False),
-                                 rtol=self.rtol,
-                                 atol=self.atol)
-        self.assert_tensor_close(self.running_stats.std(ddof=1),
-                                 input.std(dim=0, unbiased=True),
-                                 rtol=self.rtol,
-                                 atol=self.atol)
-        self.assert_tensor_close(self.running_stats.rstd(),
-                                 input.std(dim=0, unbiased=False).reciprocal(),
-                                 rtol=self.rtol,
-                                 atol=self.atol)
-        self.assert_tensor_close(self.running_stats.rstd(ddof=1),
-                                 input.std(dim=0, unbiased=True).reciprocal(),
-                                 rtol=self.rtol,
-                                 atol=self.atol)
+            self.running_rms.update(x)
+        self._verify_running_rms(input)
 
     def test_batch_update(self) -> None:
         input = torch.rand(self.outer_size, *self.inner_size)
         split_size = [1, 2, 3, 4]
-        self.running_stats.reset()
+        self.running_rms.reset()
         for x in torch.split(input, split_size):
-            self.running_stats.update(x)
-        self.assertEqual(self.running_stats.count(), self.outer_size)
-        self.assert_tensor_close(self.running_stats.mean(),
+            self.running_rms.update(x)
+        self._verify_running_rms(input)
+
+    def _verify_running_rms(self, input: torch.Tensor) -> None:
+        self.assertEqual(self.running_rms.count(), self.outer_size)
+        self.assert_tensor_close(self.running_rms.mean_square(),
+                                 input.square().mean(dim=0),
+                                 rtol=self.rtol,
+                                 atol=self.atol)
+        self.assert_tensor_close(self.running_rms.rms(),
+                                 input.square().mean(dim=0).sqrt(),
+                                 rtol=self.rtol,
+                                 atol=self.atol)
+        self.assert_tensor_close(self.running_rms.rrms(),
+                                 input.square().mean(dim=0).rsqrt(),
+                                 rtol=self.rtol,
+                                 atol=self.atol)
+
+
+class RunningMomentsTest(TestCaseBase):
+
+    def setUp(self) -> None:
+        self.outer_size = 10
+        self.inner_size = (4, 5)
+        self.running_moments = RunningMoments(self.inner_size)
+        self.rtol = 1e-6
+        self.atol = 1e-6
+
+    def test_single_update(self) -> None:
+        input = torch.rand(self.outer_size, *self.inner_size)
+        self.running_moments.reset()
+        for x in torch.unbind(input):
+            self.running_moments.update(x)
+        self._verify_running_moments(input)
+
+    def test_batch_update(self) -> None:
+        input = torch.rand(self.outer_size, *self.inner_size)
+        split_size = [1, 2, 3, 4]
+        self.running_moments.reset()
+        for x in torch.split(input, split_size):
+            self.running_moments.update(x)
+        self._verify_running_moments(input)
+
+    def _verify_running_moments(self, input: torch.Tensor) -> None:
+        self.assertEqual(self.running_moments.count(), self.outer_size)
+        self.assert_tensor_close(self.running_moments.mean(),
                                  input.mean(dim=0),
                                  rtol=self.rtol,
                                  atol=self.atol)
-        self.assert_tensor_close(self.running_stats.var(),
+        self.assert_tensor_close(self.running_moments.var(),
                                  input.var(dim=0, unbiased=False),
                                  rtol=self.rtol,
                                  atol=self.atol)
-        self.assert_tensor_close(self.running_stats.var(ddof=1),
+        self.assert_tensor_close(self.running_moments.var(ddof=1),
                                  input.var(dim=0, unbiased=True),
                                  rtol=self.rtol,
                                  atol=self.atol)
-        self.assert_tensor_close(self.running_stats.std(),
+        self.assert_tensor_close(self.running_moments.std(),
                                  input.std(dim=0, unbiased=False),
                                  rtol=self.rtol,
                                  atol=self.atol)
-        self.assert_tensor_close(self.running_stats.std(ddof=1),
+        self.assert_tensor_close(self.running_moments.std(ddof=1),
                                  input.std(dim=0, unbiased=True),
                                  rtol=self.rtol,
                                  atol=self.atol)
-        self.assert_tensor_close(self.running_stats.rstd(),
+        self.assert_tensor_close(self.running_moments.rstd(),
                                  input.std(dim=0, unbiased=False).reciprocal(),
                                  rtol=self.rtol,
                                  atol=self.atol)
-        self.assert_tensor_close(self.running_stats.rstd(ddof=1),
+        self.assert_tensor_close(self.running_moments.rstd(ddof=1),
                                  input.std(dim=0, unbiased=True).reciprocal(),
                                  rtol=self.rtol,
                                  atol=self.atol)
