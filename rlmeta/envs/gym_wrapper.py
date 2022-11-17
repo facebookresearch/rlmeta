a2cb7423ea31c2c42681b94771d66b44dc1220b4
Xiaomeng Yang <bit.yangxm@gmail.com>
2022-11-16 23:48:12 -0800

    Remove unused code

--- rlmeta/envs/gym_wrapper.py
+++ rlmeta/envs/gym_wrapper.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Any, Callable, Dict, Optional, Tuple, Union
+from typing import Callable, Optional
 
 import numpy as np
 
